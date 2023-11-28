887175952d64d971b3ad58d9f9f900c2254f660f
Matthew Frankland <admin@matthewfrankland.co.uk>
2023-11-28 19:54:11 +0000

    move to modern website

---
+++ private/ordo-1962/v1.1/db-constants.php
@@ -0,0 +1,7 @@
+<?php
+  define ( "DB_HOST", "db5013734974.hosting-data.io" );
+  define ( "DB_PORT", 3306 );
+  define ( "DB_USER", "dbu3722542" );
+  define ( "DB_PASSWORD", "tyznob-rAtfa4-jonkuk" );
+  define ( "DB_NAME", "dbs11498589" );
+?>
