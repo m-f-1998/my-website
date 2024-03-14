a78a517bea2c2d44e729755aa6f1e825a9fb0abc
Matthew Frankland <admin@matthewfrankland.co.uk>
2024-03-14 22:31:12 +0000

    Website Updates Finished

---
+++ js/main.js
@@ -0,0 +1,31 @@
+function sendEmail(event) {
+  event.preventDefault();
+  var subject = document.getElementsByName('subject')[0].value;
+  var message = document.getElementsByName('message')[0].value;
+  var email = "admin@matthewfrankland.co.uk";
+  var href = "mailto:" + email + "?subject=" + subject + "&body=" + message;
+  window.location.href = href;
+}
+addEventListener("submit", sendEmail);
+
+const navLinks = document.querySelectorAll('.nav-item')
+const menuToggle = document.getElementById('navbarSupportedContent')
+if ( navLinks.length > 0 && menuToggle ) {
+  const bsCollapse = bootstrap.Collapse.getOrCreateInstance(menuToggle, {toggle: false})
+  navLinks.forEach((l) => {
+      l.addEventListener('click', () => {
+        if ( !l.classList.contains ( "dropdown" ) ) {
+          console.log ( l.classList)
+          bsCollapse.toggle()
+        }
+      })
+  })
+}
+
+window.addEventListener ( "load", ( ) => {
+  if ( window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    document.documentElement.setAttribute('data-bs-theme','dark')
+  } else {
+    document.documentElement.setAttribute('data-bs-theme','light')
+  }
+}, { passive: true })
