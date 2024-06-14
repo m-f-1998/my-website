5e210db9a7a1384d538cc023b2531fa8c6c3da65
Matthew Frankland <admin@matthewfrankland.co.uk>
2024-06-14 16:57:33 +0100

    Added 404 htaccess redirect

--- src/matthew-frankland/src/app/app.routes.ts
+++ src/matthew-frankland/src/app/app.routes.ts
@@ -3,12 +3,7 @@ import { HttpErrorComponent } from "./http-error/http-error.component"
 import { HomeComponent } from "./home/home.component"
 
 export const routes: Routes = [
-  {
-    path: "",
-    component: HomeComponent,
-  },
-  {
-    path: "error/:code",
-    component: HttpErrorComponent,
-  }
+  { path: "", component: HomeComponent },
+  { path: "error/:code", component: HttpErrorComponent },
+  { path: "**", redirectTo: "/error/404" }
 ]
